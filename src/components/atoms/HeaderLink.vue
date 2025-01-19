6aaa2d9b96f79ff2914ce9db1e0122b8af8b73c2
casqan <61163869+casqan@users.noreply.github.com>
2025-01-19 17:37:24 +0100

    Add Tailwind CSS (#3)
    * [tailwind] Added Tailwind

---
+++ src/components/atoms/HeaderLink.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  const props = defineProps<{link:string,text:string}>()
+</script>
+
+<template>
+  <a class="ml-2 font-semibold" :href=props.link>{{props.text}}</a>
+</template>
+
+<style scoped>
+</style>
